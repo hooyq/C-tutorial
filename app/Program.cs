6cda783ee0eb705055f566423e43796f23900cbd
yenqinhoo <hyqhooyenqin@gmail.com>
2024-07-04 01:00:31 +0800

    namespace, class

--- app/Program.cs
+++ app/Program.cs
@@ -1,6 +1,40 @@
-﻿Console.WriteLine("hello world");
-Console.ReadLine();    //console will automatic close after completely execute
+﻿using System;
 
-int a = 100;
-Console.WriteLine($"the number is {a}");
-Console.ReadLine();
+class Hello
+{
+    public void wr()
+    {
+        Console.WriteLine("outside");
+        Console.ReadLine();
+    }
+}
+
+namespace Program  //a higher abstraction from a class 
+{
+    public class Program
+    {
+        static void Main(string[] args)   //only one entry is available 
+        {
+            Hello hello = new Hello();  //can be use before define
+            Hello hello2 = new Hello();
+            hello2.wr();
+            hello.wr();
+            global::Hello h = new global::Hello();   //need to use global
+            h.wr();
+        }
+    }
+
+    public class Hello
+    {
+        public void wr()
+        {
+            Console.WriteLine("inside");
+            Console.ReadLine();
+        }
+    }
+}
+
+
+
+
+
--- app/obj/Debug/net8.0/app.AssemblyInfo.cs
+++ app/obj/Debug/net8.0/app.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ecce12b82448da634f1a8faed3875fb9e98ba1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1ca2d95f8a8985ed48e4cf149a6e1294b250934")]
 [assembly: System.Reflection.AssemblyProductAttribute("app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
